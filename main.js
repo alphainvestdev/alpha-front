82ad7f1d9e5f0e19c0b418d123ca678d9c82eca6
alphainvestdev <150482692+alphainvestdev@users.noreply.github.com>
2023-11-12 02:30:54 +0100

    Fix navigation issue with fixed header (#10)
    * Fixed navigation big
    
    * Fix obsolete code
    
    ---------
    
    Co-authored-by: DjLiKiDo <djlikido@gmail.com>

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
-
 // Set top for main content
 function adjustMainMargin() {
-  var header = document.getElementById('header');
-  var main = document.querySelector('main');
+  var header = document.getElementById("header");
+  var main = document.querySelector("main");
   if (header && main) {
-      var headerHeight = header.offsetHeight;
-      main.style.marginTop = headerHeight + 'px';
+    var headerHeight = header.offsetHeight;
+    main.style.paddingTop = headerHeight + "px";
   }
 }
 
-window.onload = adjustMainMargin;
+function adjustAnchorLinkPosition() {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      var targetId = this.getAttribute("href");
 
-window.onresize = adjustMainMargin;
+      // Si el enlace es solo '#', desplazarse a la parte superior de la página
+      if (targetId === "#") {
+        e.preventDefault();
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+      // Para otros enlaces de anclaje, ajustar la posición de desplazamiento
+      else {
+        e.preventDefault();
+        var targetElement = document.querySelector(targetId);
+        if (targetElement) {
+          var headerHeight = document.getElementById("header").offsetHeight;
+          var targetPosition =
+            targetElement.getBoundingClientRect().top +
+            window.scrollY -
+            headerHeight;
+
+          window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth",
+          });
+        }
+      }
+    });
+  });
+}
+
+// Evento que se dispara cuando la página se carga completamente
+window.onload = function () {
+  // adjustMainMargin();
+  adjustAnchorLinkPosition(); // Llama a la función para ajustar los enlaces de anclaje
+};
+
+// Evento que se dispara cuando la ventana cambia de tamaño
+window.onresize = function () {
+  // adjustMainMargin();
+  adjustAnchorLinkPosition();
+};
